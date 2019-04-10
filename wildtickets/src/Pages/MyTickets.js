9ba3c778807ff9f2c3e8801e533c42a967d1752c
Samir <samir.fennikh@gmail.com>
2019-04-10 17:35:32 +0200

    Adding router and nav.

---
+++ wildtickets/src/Pages/MyTickets.js
@@ -0,0 +1,28 @@
+import React, { Component, Fragment } from 'react';
+// import { Redirect } from 'react-router-dom'
+import Header from '../components/Header';
+import {
+    Container
+} from 'reactstrap';
+
+class MyTickets extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Header />
+                <Container className="bg-to-delete">
+                    <h1>My tickets</h1>
+                </Container>
+            </Fragment>
+        );
+    }
+}
+
+export default MyTickets;
