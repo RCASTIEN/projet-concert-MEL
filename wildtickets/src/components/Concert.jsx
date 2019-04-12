b19019df69e9eeee4a7263646f6d095f760ddec2
Loukiana VIVENOT <kanaliou@hotmail.fr>
2019-04-12 11:56:20 +0200

    adding h6 Date concert in concert component AND import Concert component in LastConcertFavoriteBands

--- wildtickets/src/components/Concert.jsx
+++ wildtickets/src/components/Concert.jsx
@@ -20,7 +20,10 @@ class Concert extends React.Component {
                         <CardImg className="oblique-img" top width="100%" src="https://images.pexels.com/photos/1763075/pexels-photo-1763075.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Card image cap" />
                     </div>
                     <CardBody>
-                        <CardTitle><h4 className="ellips-title">Concert Name</h4></CardTitle>
+                        <CardTitle>
+                            <h4 className="ellips-title">Concert Name</h4>
+                            <h6 className="ellips-title">Concert Date</h6>
+                        </CardTitle>
                         <Button className="discover-btn">More Information</Button>
                     </CardBody>
               </Card>
