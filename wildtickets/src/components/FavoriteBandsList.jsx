4fa29fc76ed8f70fe6dd7f94e9f4cb26c2b3dd9a
Marine Sausse <saussemarine@gmail.com>
2019-04-12 15:09:55 +0200

    Adding horizontal scrollbar to FavoriteBandsList.

--- wildtickets/src/components/FavoriteBandsList.jsx
+++ wildtickets/src/components/FavoriteBandsList.jsx
@@ -2,26 +2,37 @@ import React, {Fragment} from 'react';
 import Bands from './Bands';
 import { Row } from 'reactstrap';
 import Container from 'reactstrap/lib/Container';
+import '../styles/main.css';
 
 class FavoriteBandsList extends React.Component {
-    constructor(props) {
-        super(props)
+   constructor(props) {
+       super(props)
 
-    }
-    render() {
-        return (
+   }
+   render() {
+       return (
             <Fragment>
                 <Container>
-                    <Row>
-                        <Bands />
-                        <Bands />
-                        <Bands />
-                        <Bands />
+                    <Row className='scrolling-wrapper-flexbox'>
+                        <Bands className='card'/>
+                        <Bands className='card'/>
+                        <Bands className='card'/>
+                        <Bands className='card'/>
+                        <Bands className='card'/>
+                        <Bands className='card'/>
+                        <Bands className='card'/>
+                        <Bands className='card'/>
+                        <Bands className='card'/>
+                        <Bands className='card'/>
+                        <Bands className='card'/>
+                        <Bands className='card'/>
+                        <Bands className='card'/>
+                        <Bands className='card'/>
                     </Row>
                 </Container>
             </Fragment>
-        )
-    }
+       )
+   }
 };
 
 export default FavoriteBandsList;
