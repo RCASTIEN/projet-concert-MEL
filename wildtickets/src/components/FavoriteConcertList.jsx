398422266790a4f2060ceefd209116346156cef4
Loukiana VIVENOT <kanaliou@hotmail.fr>
2019-04-12 11:09:24 +0200

    Create FavoriteConcertList component

---
+++ wildtickets/src/components/FavoriteConcertList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class FavoriteConcertList extends React.Component {
+    constructor(props) {
+        super(props)
+
+    }
+    render() {
+        return (
+            <div>
+                Favorite concert
+            </div>
+        )
+    }
+};
+
+export default FavoriteConcertList;
