64321de1ffa39124fa9e75ce7c7d59a76cbf216b
Loukiana VIVENOT <kanaliou@hotmail.fr>
2019-04-12 11:10:10 +0200

    Create LastConcertFavoriteBands component

---
+++ wildtickets/src/components/LastConcertFavoriteBands.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class LastConcertFavoriteBands extends React.Component {
+    constructor(props) {
+        super(props)
+
+    }
+    render() {
+        return (
+            <div>
+                Last concert
+            </div>
+        )
+    }
+};
+
+export default LastConcertFavoriteBands;
