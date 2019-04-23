aa617c087a5daf051d0b2c61a967bef3532e6a1c
Richard Gaillet <gaillet.richard@gmail.com>
2019-04-23 17:10:00 +0200

    Add Contact, Sitemap & Terms of use pages.

---
+++ wildtickets/src/Pages/Contact.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+class Contact extends React.Component {
+    render(){
+        return(
+            <div>
+                <Header />
+                Contact
+                <Footer />
+            </div>
+        )
+    }
+}
+
+export default Contact;
