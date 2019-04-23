ec1bd07c8bf30455cd7295ac43cf488b4c319556
Loukiana VIVENOT <kanaliou@hotmail.fr>
2019-04-23 10:47:04 +0200

    creating handleAlert Function

--- wildtickets/src/components/Bands.jsx
+++ wildtickets/src/components/Bands.jsx
@@ -24,23 +24,29 @@ class Bands extends React.Component {
         e.preventDefault();
         this.setState({
             addToFavorite: !this.state.addToFavorite,
-        })
+        },()=>{
+            this.props.alertFunction("...");
+        }
+        );
+        
     }
 
     render() {
         return (
-            <Col className="grid__item">
-                <Card>
-                <div className="img-container">
-                    <CardImg className="oblique-img" top width="100%" src="https://images.pexels.com/photos/1763075/pexels-photo-1763075.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Card image cap" />
-                </div>
-                <CardBody>
-                    <CardTitle><h4 className="ellips-title">Artist Name</h4></CardTitle>
-                    <Heart onClick={this.handleAddToFavorite} className={this.state.addToFavorite ? "heart-filled" : "heart-little-card" } />
-                    <Button className="discover-btn">DISCOVER</Button>
-                </CardBody>
-                </Card>
-            </Col>
+            <React.Fragment>
+                <Col className="grid__item">
+                    <Card>
+                    <div className="img-container">
+                        <CardImg className="oblique-img" top width="100%" src="https://images.pexels.com/photos/1763075/pexels-photo-1763075.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Card image cap" />
+                    </div>
+                    <CardBody>
+                        <CardTitle><h4 className="ellips-title">Artist Name</h4></CardTitle>
+                        <Heart onClick={this.handleAddToFavorite} className={this.state.addToFavorite ? "heart-filled" : "heart-little-card" } />
+                        <Button className="discover-btn">DISCOVER</Button>
+                    </CardBody>
+                    </Card>
+                </Col>
+            </React.Fragment>
         )
     }
 };
--- wildtickets/src/components/FavoriteBandsList.jsx
+++ wildtickets/src/components/FavoriteBandsList.jsx
@@ -1,38 +1,65 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import Bands from './Bands';
 import { Row } from 'reactstrap';
 import Container from 'reactstrap/lib/Container';
 
 class FavoriteBandsList extends React.Component {
-   constructor(props) {
-       super(props)
+    constructor(props) {
+        super(props)
+        this.state={
+            alert: false,
+            message: undefined
+        }
+        this.handleAlert = this.handleAlert.bind(this);
+    }
 
-   }
-   render() {
-       return (
+    handleAlert(msg) {
+        //console.log("toto!");
+        //e.preventDefault();
+        this.setState({
+            alert: true,
+            message: msg
+        });
+        
+        setTimeout(()=>{
+            this.setState({
+                alert: false,
+                message: undefined
+            });
+        },2000);
+    }
+    render() {
+        return (
             <Fragment>
+                {
+                    this.state.alert && 
+                        <div class="alert alert-danger" role="alert">
+                            This is a danger alert—check it out!
+                        </div>
+
+                }
                 <Container>
-                <h1>Favorite Bands</h1>
+                    <h1>Favorite Bands</h1>
                     <Row className='scrolling-wrapper-flexbox grid grid-xs--scroll grid-xs-2--scroll'>
-                        <Bands className='card'/>
-                        <Bands className='card'/>
-                        <Bands className='card'/>
-                        <Bands className='card'/>
-                        <Bands className='card'/>
-                        <Bands className='card'/>
-                        <Bands className='card'/>
-                        <Bands className='card'/>
-                        <Bands className='card'/>
-                        <Bands className='card'/>
-                        <Bands className='card'/>
-                        <Bands className='card'/>
-                        <Bands className='card'/>
-                        <Bands className='card'/>
+                        <Bands className='card' alertFunction={this.handleAlert} />
+                        <Bands className='card' />
+                        <Bands className='card' />
+                        <Bands className='card' />
+                        <Bands className='card' />
+                        <Bands className='card' />
+                        <Bands className='card' />
+                        <Bands className='card' />
+                        <Bands className='card' />
+                        <Bands className='card' />
+                        <Bands className='card' />
+                        <Bands className='card' />
+                        <Bands className='card' />
+                        <Bands className='card' />
                     </Row>
                 </Container>
             </Fragment>
-       )
-   }
+        )
+    }
 };
 
 export default FavoriteBandsList;
