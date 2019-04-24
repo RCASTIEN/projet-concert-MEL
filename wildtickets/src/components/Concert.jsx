5d8575285a806c3b639662f5429d5edbfb01fc79
Richard Gaillet <gaillet.richard@gmail.com>
2019-04-24 09:20:52 +0200

    Delete unless constructor.

--- wildtickets/src/components/Concert.jsx
+++ wildtickets/src/components/Concert.jsx
@@ -9,10 +9,6 @@ import {
   } from 'reactstrap';
 
 class Concert extends React.Component {
-    constructor(props) {
-        super(props)
-
-    }
     render() {
         return (
             <div>
