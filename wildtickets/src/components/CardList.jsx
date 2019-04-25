2ed161cb0d8f6c32e6620c9571ff598f9da5432d
Samir <samir.fennikh@gmail.com>
2019-04-25 10:51:20 +0200

    testing filter by place

--- wildtickets/src/components/CardList.jsx
+++ wildtickets/src/components/CardList.jsx
@@ -8,11 +8,12 @@ class CardList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      eventCard: []
+      eventCard: [],
+      metroArea: 28909
     }
   }
   componentDidMount() {
-    axios.get("https://api.songkick.com/api/3.0/metro_areas/28886/calendar.json?apikey=5yrQwIh2tGWNTggG").then(res => {
+    axios.get(`https://api.songkick.com/api/3.0/metro_areas/${this.state.metroArea}/calendar.json?apikey=5yrQwIh2tGWNTggG`).then(res => {
       this.setState({
         eventCard: res.data.resultsPage.results.event
       });
@@ -20,24 +21,24 @@ class CardList extends Component {
   }
 
   render() {
-  
+
     return (
       <Container>
-          {this.state.eventCard.map((eventCardLists, i) => {
-            return (eventCardLists.performance[0]
-              &&
-              <HorizontalCard
-                key={i}
-                name={eventCardLists.performance[0].displayName}
-                avatar={eventCardLists.performance[0].artist.id}
-                venue={eventCardLists.venue.displayName}
-                date={eventCardLists.start.date}
-                link={eventCardLists.performance[0].artist.id}
-                fav={eventCardLists.id}
+        {this.state.eventCard.map((eventCardLists, i) => {
+          return (eventCardLists.performance[0]
+            &&
+            <HorizontalCard
+              key={i}
+              name={eventCardLists.performance[0].displayName}
+              avatar={eventCardLists.performance[0].artist.id}
+              venue={eventCardLists.venue.displayName}
+              date={eventCardLists.start.date}
+              link={eventCardLists.performance[0].artist.id}
+              fav={eventCardLists.id}
 
-              />
-            )
-          })}
+            />
+          )
+        })}
       </Container>
     );
   }
