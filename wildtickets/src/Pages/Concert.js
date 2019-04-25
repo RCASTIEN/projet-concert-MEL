94e8088d4d987b200ccd25640a6c165007a2fbf2
Richard Gaillet <gaillet.richard@gmail.com>
2019-04-25 14:07:32 +0200

    Add API call.

--- wildtickets/src/Pages/Concert.js
+++ wildtickets/src/Pages/Concert.js
@@ -1,15 +1,34 @@
 import React, { Component, Fragment } from 'react';
 import Header from '../components/Header';
+import axios from "axios";
 import '../styles/Concert.css';
 
 class Concerts extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            concertArray: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get("https://api.songkick.com/api/3.0/artists/558710/calendar.json?apikey=5yrQwIh2tGWNTggG")
+            .then(res => {
+                this.setState({
+                    concertsArray: res.data.resultsPage.results.event
+                });
+                console.log(this.state.concertsArray);
+                console.log(this.state.concertsArray[0].performance[0].artist.id);
+            });
+    };
+
     render() {
         return (
             <Fragment>
                 <Header />
-                <img className="oblique" src="https://images.sk-static.com/images/media/profile_images/artists/558710/huge_avatar" alt="" title="" />
-                <img className="cercle" src="https://images.sk-static.com/images/media/profile_images/artists/558710/huge_avatar" alt="" title="" />
-                <p className="text-center">Concert</p>
+                <img className="oblique" src="https://images.sk-static.com/images/media/profile_images/artists/558710/huge_avatar" alt="" title="ARTIST" />
+                <img className="cercle" src="https://images.sk-static.com/images/media/profile_images/artists/558710/huge_avatar" alt="" title="CONCERT" />
+                <p className="text-center">Concerts de {this.state.concertArray}</p>
             </Fragment>
         );
     };
